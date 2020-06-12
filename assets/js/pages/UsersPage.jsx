2cc45b42c2d4d8db1fc5a65f0787df364125cbf4
Xavier BURKE <xavier.burke@gmail.com>
2020-06-12 11:52:19 +0200

    React - pages Utilisateur

---
+++ assets/js/pages/UsersPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <div className="alert alert-dismissible alert-light">
+                <h1><strong>Bienvenue</strong> sur la page des utilisateurs de la plateforme</h1><hr/>A partir de ce Tableau de bord, vous pourrez administrer la vidéothèque, ...
+            </div>
+        </div>
+    );
+};
+
+export default DashboardPage;
